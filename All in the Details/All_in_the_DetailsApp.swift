748792f23c35fa07de994c7602bf03c0b020983a
amstanage <stonage3@gmail.com>
2026-03-05 14:46:21 -0500

    Initial Commit

---
+++ All in the Details/All_in_the_DetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_in_the_DetailsApp.swift
+//  All in the Details
+//
+//  Created by Alex Stanage on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct All_in_the_DetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ All in the Details/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  All in the Details
+//
+//  Created by Alex Stanage on 3/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
